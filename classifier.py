a411f663e1a156960cafe7939ee6e94328f33c69
yaoplusplus <ljy5799@outlook.com>
2023-11-08 11:24:31 +0800

    为FlowPicNet添加mode：feature_extractor，添加分类器PureClassifier

--- classifier.py
+++ classifier.py
@@ -1,14 +1,19 @@
 import torch
 import torch.nn as nn
-from torch.nn import Conv2d, MaxPool2d, Flatten, Linear, Dropout, ReLU, Softmax, Sequential
+from torch.nn import Conv2d, MaxPool2d, Flatten, Linear, Dropout, ReLU, Sequential
 import torchvision.models as models
 from pprint import pprint
+from sklearn.decomposition import PCA
 
 
 class FlowPicNet(nn.Module):
-    # input [1,1,1500,1500]
-    def __init__(self, num_classes, show_temp_out=False):
+    # input [batch_size,1,1500,1500]
+    def __init__(self, num_classes, show_temp_out=False, mode=None):
+        """
+        mode : feature_extractor-提取特征向量, classifier-分类器, None-全流程
+        """
         super().__init__()
+        self.mode = mode
         self.num_classes = num_classes
         self.show_temp_out = show_temp_out
         # stride >1 的时候，pytorch不允许使用 padding="same"
@@ -19,39 +24,60 @@ class FlowPicNet(nn.Module):
         self.maxpool2 = MaxPool2d(kernel_size=2)
         self.flatten = Flatten()
         self.linear1 = Sequential(Linear(in_features=4500, out_features=64), ReLU(), Dropout(0.5))
-        # self.linear2 = Sequential(Linear(in_features=64, out_features=self.num_classes), Softmax())
+        # self.linear2 = Sequential(Linear(in_features=64, out_features=self.num_classes))
         self.linear2 = Sequential(Linear(in_features=64, out_features=self.num_classes))
 
+        self.extractor = Sequential(self.conv1, self.maxpool1, self.conv2, self.drop1, self.maxpool2, self.flatten)
+        self.classifier = Sequential(self.linear1, self.linear2)
+
     def forward(self, x):
-        x = self.conv1(x)
-        if self.show_temp_out:
-            print('after conv1: ', x.shape)
-        x = self.maxpool1(x)
-        if self.show_temp_out:
-            print('after maxpool1: ', x.shape)
-        x = self.conv2(x)
-        if self.show_temp_out:
-            print('after conv2: ', x.shape)
-        x = self.drop1(x)
+        if self.mode == 'feature_extractor':
+            return self.extractor(x)
+        if self.mode == 'classifier':
+            return self.classifier(x)
+        else:
+            shapes = {}
+            x = self.conv1(x)
+            shapes['conv1'] = x.shape
+            x = self.maxpool1(x)
+            shapes['maxpool1'] = x.shape
+            x = self.conv2(x)
+            shapes['conv2'] = x.shape
+            x = self.drop1(x)
+            x = self.maxpool2(x)
+            shapes['maxpool2'] = x.shape
+            # 展平，解决batch——bug的地方
+            x = self.flatten(x)
+            shapes['flatten'] = x.shape
+            x = self.linear1(x)
+            shapes['linear1'] = x.shape
+            x = self.linear2(x)
+            shapes['linear2'] = x.shape
 
-        x = self.maxpool2(x)
-        if self.show_temp_out:
-            print('after maxpool2: ', x.shape)
-        # 展平，解决batch——bug的地方
-        x = self.flatten(x)
-        if self.show_temp_out:
-            print('after flatten: ', x.shape)
-        x = self.linear1(x)
-        if self.show_temp_out:
-            print('after linear1: ', x.shape)
-        x = self.linear2(x)
-        if self.show_temp_out:
-            print('after linear2: ', x.shape)
+            if self.show_temp_out:
+                print(shapes)
+            return x
 
-        return x
     def name(self):
         base = 'FlowPicNet'
         return base
+
+
+class PureClassifier(nn.Module):
+    def __init__(self, in_features, num_classes):
+        super().__init__()
+        self.num_classes = num_classes
+        self.linear1 = Sequential(Linear(in_features=in_features, out_features=64), ReLU(), Dropout(0.5))
+        self.linear2 = Sequential(Linear(in_features=64, out_features=self.num_classes))
+
+    def forward(self, x):
+        x = self.linear1(x)
+        x = self.linear2(x)
+        return x
+
+    def name(self):
+        return 'PureClassifier'
+
 
 class LeNet(nn.Module):
     def __init__(self, num_classes):
@@ -86,27 +112,46 @@ class FlowPicNet_adaptive(FlowPicNet):
     256*256   -> 90
     """
 
-    def __init__(self, num_classes, show_temp_out=False,liner1_in_feature=4500):
+    def __init__(self, num_classes, show_temp_out=False, liner1_in_feature=4500):
         super().__init__(num_classes=num_classes, show_temp_out=show_temp_out)
         self.linear1 = Sequential(Linear(in_features=liner1_in_feature, out_features=64), ReLU(), Dropout(0.5))
 
 
 def resnet(num_classes):
-    # todo: add softmax func ?
     net = models.resnet18(weights=None)
     net.conv1 = Conv2d(in_channels=1, out_channels=64, kernel_size=7, stride=2, padding=2, bias=False)
     net.fc = Linear(in_features=512, out_features=num_classes, bias=True)
     return net
 
 
-# class FlowPicNet_256_Reduce(FlowPicNet_256):
-    """
-    在FlowPicNet_256的基础上去掉了self.drop1 和self.linear1的drop_out
-    """
+def reduce_dim(x):
+    estimator = PCA(n_components=8)
+    x = estimator.fit_transform(x)
+    print(x.shape)
+    pass
 
+
+class FlowPicNet_32(nn.Module):
     def __init__(self, num_classes, show_temp_out=False):
-        super().__init__(num_classes=num_classes, show_temp_out=show_temp_out)
-        self.linear1 = Sequential(Linear(in_features=80, out_features=64), ReLU())
+        super().__init__()
+        self.num_classes = num_classes
+        self.show_temp_out = show_temp_out
+        # stride >1 的时候，pytorch不允许使用 padding="same"
+        self.conv1 = Sequential(Conv2d(in_channels=1, out_channels=6, kernel_size=9, stride=1, padding=2), ReLU())
+        # 6*28*28
+        self.maxpool1 = MaxPool2d(kernel_size=2)
+        # 6*14*14
+        self.conv2 = Sequential(Conv2d(in_channels=6, out_channels=16, kernel_size=9, stride=1, padding=2),
+                                ReLU())
+        # 6*10*10
+        self.drop1 = Dropout(0.25)
+        self.maxpool2 = MaxPool2d(kernel_size=2)
+        # 6*5*5
+        # paper里提到了PAC
+        self.flatten = Flatten()
+        self.linear1 = Sequential(Linear(in_features=120, out_features=120), ReLU(), Dropout(0.5))
+        # self.linear2 = Sequential(Linear(in_features=64, out_features=self.num_classes), Softmax())
+        self.linear2 = Sequential(Linear(in_features=84, out_features=self.num_classes))
 
     def forward(self, x):
         x = self.conv1(x)
@@ -118,6 +163,8 @@ def resnet(num_classes):
         x = self.conv2(x)
         if self.show_temp_out:
             print('after conv2: ', x.shape)
+        x = self.drop1(x)
+
         x = self.maxpool2(x)
         if self.show_temp_out:
             print('after maxpool2: ', x.shape)
@@ -134,11 +181,20 @@ def resnet(num_classes):
 
         return x
 
+    def name(self):
+        return 'FlowPicNet_32'
+
+
+class JointFlowPicNet(nn.Module):
+    pass
+
 
 if __name__ == '__main__':
-    # code for test ResNet()
-    model = FlowPicNet_adaptive(num_classes=4, show_temp_out=True, liner1_in_feature=9000)
-    t = torch.unsqueeze(torch.rand([1, 3000, 1500]), 0)
-    print(model(t))
-    # pprint(model(t))
-    pass
+    # 测试模型输出
+    model = FlowPicNet(num_classes=4, show_temp_out=False)
+    # t = torch.unsqueeze(torch.rand([1, 1500, 1500]), 0)
+    t = torch.rand([1, 1, 1500, 1500])
+    print(model(t).shape)
+    # pass
+    # tensor = torch.rand([16, 25])
+    # reduce_dim(tensor)
