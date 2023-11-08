375997a376bef69791440daf9da87b88d86e4bbf
yaoplusplus <ljy5799@outlook.com>
2023-11-08 11:26:23 +0800

    添加了生成JointFeature数据集的功能

--- utils.py
+++ utils.py
@@ -1,11 +1,19 @@
+import glob
 import os
 import time
+from typing import List
 
+import numpy as np
 import torch
 import yaml
 from matplotlib import pyplot as plt
+from tqdm import tqdm
+
+from classifier import FlowPicNet
 from mydataset import SimpleDataset
 from torch.utils.data import DataLoader
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 def load_config_from_yaml(file_path):
@@ -59,9 +67,10 @@ def show_tensor(tensor: torch.Tensor):
     plt.show()
 
 
-def get_dataloader_datasetname_numclasses(dataset: str, feature_method: str, batch_size: int = 128, shuffle: bool = True):
+def get_dataloader_datasetname_numclasses(root, dataset: str, feature_method: str, batch_size: int = 128,
+                                          shuffle: bool = True):
     # 从utils.py文件所在位置锚定数据集位置
-    root = os.path.join(os.path.dirname(__file__), 'dataset', 'processed')
+    # root = os.path.join(os.path.dirname(__file__), 'dataset', 'processed') ISCX*** 's dir
     train_dataset = SimpleDataset(
         dataset=dataset, feature_method=feature_method, root=root, train=True)
     test_dataset = SimpleDataset(
@@ -69,9 +78,94 @@ def get_dataloader_datasetname_numclasses(dataset: str, feature_method: str, bat
     train_loader = DataLoader(dataset=train_dataset,
                               batch_size=batch_size, shuffle=shuffle)
     test_loader = DataLoader(dataset=test_dataset,
-                             batch_size=batch_size, shuffle=shuffle)
+                             batch_size=batch_size, shuffle=False)
     return train_loader, test_loader, train_dataset.name(), train_dataset.get_num_classes()
 
 
+def get_num_classes(path):
+    dirs_count = 0
+    files_or_dirs = glob.glob(os.path.join(path, '*'))
+    for file_or_dir in files_or_dirs:
+        if os.path.isdir(file_or_dir):
+            dirs_count += 1
+    return dirs_count
+
+
+datasets = {
+    'ISCXVPN2016_VPN': {
+        'dataset_dir': './dataset/raw/ISCXVPN2016_VPN',
+        'features_dir': './dataset/processed/ISCXVPN2016_VPN',
+        'classes': {'video-streaming': 0, 'chat': 1, 'email': 2, 'ftp': 3, 'p2p': 4, 'voip': 5},
+        'ignore_files': ['vpn_spotify_A.pcap']
+    },
+    # ISCXTor2016 https://www.unb.ca/cic/datasets/tor.html
+    'ISCXTor2016_tor': {
+        'dataset_dir': './dataset/raw/ISCXTor2016_tor',
+        'features_dir': './dataset/processed/ISCXTor2016_tor',
+        'classes': {'browsing': 0, 'chat': 1, 'email': 2, 'ftp': 3, 'p2p': 4, 'audio': 5, 'voip': 6, 'video': 7},
+        'ignore_files': []
+    },
+
+    'ISCXTor2016_nonTor': {
+        'dataset_dir': './dataset/raw/ISCXTor2016_nonTor',
+        'features_dir': './dataset/processed/ISCXTor2016_nonTor',
+        'classes': {'browsing': 0, 'chat': 1, 'email': 2, 'ftp': 3, 'p2p': 4, 'voip': 5},
+        # streaming -AUDIO and VIDEO
+        'ignore_files': ['ssl.pcap', 'spotify.pcap', 'spotify2.pcap', 'spotify2-1.pcap', 'spotify2-2.pcap',
+                         'spotifyAndrew.pcap']
+        # 实际上我将这些文件暂存在一个子文件夹里了
+    },
+}
+
+
+def make_joint_features(root: str, dataset: str, feature_methods: List, feature_extractor: str = 'FlowPicNet',
+                        para_dict: str = None,
+                        ):
+    """
+    提取特征向量并保存
+    root : like '/home/cape/data/trace/new_processed',
+    dataset: dataset name like 'ISCXTor2016_tor'
+    feature_method: FlowPic or MyFlowPic
+    para_dict: 模型参数文件 like './90.89.pth'
+    feature_extractor: 模型类名称 like 'FlowPicNet', 'LeNet'
+    """
+    path = os.path.join(root, dataset, feature_methods[0])
+    assert os.path.exists(path), '不存在的路径'
+    flowpic_files = glob.glob(f'{path}/*/*.npz')
+
+    # 特征提取器
+    feature_extractor: torch.nn.Module = eval(feature_extractor)(num_classes=get_num_classes(path),
+                                                                 mode='feature_extractor').to(device)
+    if para_dict:
+        feature_extractor.load_state_dict(torch.load(para_dict))
+    feature_extractor.eval()
+
+    for file in tqdm(flowpic_files):
+        split_file_path = file.split('/')
+        # load hist and label
+        # label = datasets[dataset]['classes'](split_file_path[-2])
+        # [1500,1500] -> [1,1,1500,1500]
+        flowpic = torch.Tensor(np.load(file)['flowpic'].astype(float)).to(device).unsqueeze(dim=0).unsqueeze(dim=0)
+        myflowpic = torch.Tensor(np.load(file.replace(feature_methods[0], feature_methods[1]))['flowpic'].astype(float)).to(
+            device).unsqueeze(dim=0).unsqueeze(dim=0)
+
+        # 获取并串联特征
+        with torch.no_grad():
+            flowpic_feature = feature_extractor(flowpic)
+            myflowpic_feature = feature_extractor(myflowpic)
+            joint_fature = torch.concat([flowpic_feature, myflowpic_feature], dim=1)
+            # save file
+            save_path = os.path.join(root, dataset, 'JointFeature', split_file_path[-2])
+            os.makedirs(save_path, exist_ok=True)
+            np.savez_compressed(os.path.join(save_path, split_file_path[-1]), feature=joint_fature.cpu().numpy())
+
+
 if __name__ == '__main__':
+    make_joint_features(root='/home/cape/data/trace/new_processed', dataset='ISCXVPN2016_VPN',
+                        feature_methods=['FlowPic', 'MyFlowPic'],
+                        # para_dict='/home/cape/code/FlowPic/checkpoints/FlowPicNet-ISCXTor2016_tor_FlowPic-Adam-ReduceLROnPlateau-2023-11-07_17-55-25/0.8546.pth',
+                        feature_extractor='FlowPicNet')
+    # feature = np.load(
+    #     '/home/cape/code/FlowPic/dataset/processed/ISCXTor2016_tor/JointFeature/audio/flowpic-1437494738355-10.0.2.15-57188-82.161.239.177-110-6-src2dst.npz')['feature']
+    # print(feature.shape)
     pass
